8cff4bd2ecc001102a5df562a94f7191c801a28c
#StopHazaraGenocide <118218217+hazaristan@users.noreply.github.com>
2022-11-16 12:04:11 +0330

    fix a bug

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // dark mode and lite mode
-const telegram = document.querySelector('.telegram')
-const darkLite = document.querySelector('.darkLite')
-const language = document.querySelector('.language')
-const tweet = document.querySelector('.tweet')
-const refresh = document.querySelector('.refresh')
+const lite = document.querySelector('.lite')
+const dark = document.querySelector('.dark')
 const h1 = document.querySelector('h1')
 const p = document.querySelector('p')
 const darkColor = '#2B2A33'
@@ -11,21 +8,15 @@ const liteColor = '#F5F5F5'
 let isLiteMode = 'on'
 function darkLiteMode() {
   if (isLiteMode === 'on') {
-    telegram.style.backgroundImage = "url('img/telegram-lite.webp')"
-    darkLite.style.backgroundImage = "url('img/darklite-lite.webp')"
-    language.style.backgroundImage = "url('img/language-lite.webp')"
-    tweet.style.backgroundImage = "url('img/tweet-lite.webp')"
-    refresh.style.backgroundImage = "url('img/refresh-lite.webp')"
+    lite.style.display='none'
+    dark.style.display='flex'
     document.body.style.background = liteColor
     h1.style.color = darkColor
     p.style.color = darkColor
     isLiteMode = 'off'
   } else {
-    telegram.style.backgroundImage = "url('img/telegram-dark.webp')"
-    darkLite.style.backgroundImage = "url('img/darklite-dark.webp')"
-    language.style.backgroundImage = "url('img/language-dark.webp')"
-    tweet.style.backgroundImage = "url('img/tweet-dark.webp')"
-    refresh.style.backgroundImage = "url('img/refresh-dark.webp')"
+    dark.style.display='none'
+    lite.style.display='flex'
     document.body.style.background = darkColor
     h1.style.color = liteColor
     p.style.color = liteColor
@@ -38,7 +29,8 @@ darkLiteMode()
 let isLanguage = 'farsi'
 let rtl = ['farsi', 'arabic']
 function changeLanguage(lang) {
-  language.innerHTML = isLanguage.slice(0, 2)
+  document.querySelector('.lite>.language').innerHTML = isLanguage.slice(0, 2)
+  document.querySelector('.dark>.language').innerHTML = isLanguage.slice(0, 2)
   rtl.indexOf(lang) ? p.style.textAlign = 'left' : p.style.textAlign = 'right'
   isLanguage = lang
   addTweet()
