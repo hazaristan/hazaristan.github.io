894c7bd9273d942c0ef05c9383e6cb9bb9b4b910
#StopHazaraGenocide <118218217+hazaristan@users.noreply.github.com>
2022-11-21 14:40:52 +0330

    add smtpjs librariy for contact form

--- cooperate/script.js
+++ cooperate/script.js
@@ -1,11 +1,10 @@
 const telegram = document.querySelector('.telegram')
 const h1 = document.querySelector('h1')
 const p = document.querySelector('p')
-const span = document.querySelector('span')
-const pForm = document.querySelector('#my-form-status')
+const result = document.querySelector('.result')
 const email = document.querySelector('.email')
 const textarea = document.querySelector('textarea')
-const button = document.querySelector('button')
+const button = document.querySelector('.button')
 const formDarkColor = '#42414d'
 const darkColor = '#2B2A33'
 const liteColor = '#F5F5F5'
@@ -16,59 +15,41 @@ function darkLiteMode() {
     document.body.style.background = darkColor
     h1.style.color = liteColor
     p.style.color = liteColor
-    span.style.color = liteColor
-    pForm.style.color = liteColor
+    result.style.color = liteColor
     email.style.color = liteColor
     email.style.background = formDarkColor
     textarea.style.color = liteColor
     textarea.style.background = formDarkColor
     button.style.background = formDarkColor
     button.style.color = liteColor
-    localStorage.setItem('darkLiteMode','off')
   }
 }
 darkLiteMode()
 
-var characterNumber
+let characterNumber
 function characterLength() {
   characterNumber = 258 - textarea.value.length
-  span.innerText = 'کاراکتر مجاز : ' + characterNumber
-  if (characterNumber === 0){
+  result.innerText = 'کاراکتر مجاز : ' + characterNumber
+  if (characterNumber === 0) {
     textarea.style.color = 'red'
-    span.innerText = 'کاراکتر مجاز به پایان رسید'
-  }else{
+    result.innerText = 'کاراکتر مجاز به پایان رسید'
+  } else {
     isLiteMode === 'on' ? textarea.style.color = darkColor : textarea.style.color = liteColor
   }
 }
 
-// formspree code for form 
-var form = document.getElementById("my-form");
-
-async function handleSubmit(event) {
-  event.preventDefault();
-  var status = document.getElementById("my-form-status");
-  var data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-      'Accept': 'application/json'
-    }
-  }).then(response => {
-    if (response.ok) {
-      status.innerHTML = "پیامتون ارسال شد ، از همکاریتون ممنونم";
-      form.reset()
-    } else {
-      response.json().then(data => {
-        if (Object.hasOwn(data, 'errors')) {
-          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-        } else {
-          status.innerHTML = "ارسال پیام با مشکل مواجه شد"
-        }
-      })
-    }
-  }).catch(error => {
-    status.innerHTML = "ارسال پیام با مشکل مواجه شد"
-  });
+function sendMail() {
+  result.innerText = 'در حال ارسال'
+  textarea.value = textarea.value.replaceAll(`
+`, '<br>')
+  Email.send({
+    SecureToken: "eed1466e-19c8-4d32-a804-511a649d2c88",
+    To: 'hazaristangithubio@gmail.com',
+    From: "hazaristangithubio@gmail.com",
+    Subject: "A new tweet for website",
+    Body: `Email : ${email.value} <br> ${textarea.value}`
+  }).then(
+    message => result.innerText = message === 'OK' ? 'پیامتون ارسال شد ، از همکاریتون ممنونم' : 'ارسال پیام با مشکل مواجه شد'
+  );
+  isLiteMode === 'on' ? textarea.style.color = darkColor : textarea.style.color = liteColor
 }
-form.addEventListener("submit", handleSubmit)
